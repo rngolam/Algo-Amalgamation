5b08cfdb2c856ed5bc8a7ce6afd0ff8dd86e38ac
Richard Ngo-Lam <rngolam@gmail.com>
2021-05-24 23:25:31 -0500

    style buttons

--- script/carousel.js
+++ script/carousel.js
@@ -1,34 +1,32 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+var slideIndex = 1;
+showSlides(slideIndex);
 
-  var slideIndex = 1;
-  showSlides(slideIndex);
+debugger;
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-  }
-
-  // Autoplay functionality
-  setInterval(function(){plusSlides(1)}, 4500);
-})
+// Autoplay functionality
+setInterval(function(){plusSlides(1)}, 4500);
